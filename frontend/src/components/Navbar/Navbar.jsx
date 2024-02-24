5176272bce336d4f435b6b61cebaa596177cf397
Nando <fbmenezes1@hotmail.com>
2024-02-24 15:50:18 -0300

    Title change

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './Navbar.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="nav-container">
-      <h1 className="nav-title">Astronomy App</h1>
+      <h1 className="nav-title">Astro Explore</h1>
       <div className="nav-links">
-        <NavLink to="/" exact className="nav-link" activeClassName="active-link">
+        <NavLink
+          to="/"
+          exact
+          className="nav-link"
+          activeClassName="active-link"
+        >
           Home
         </NavLink>
-        <NavLink to="/observatory" className="nav-link" activeClassName="active-link">
+        <NavLink
+          to="/observatory"
+          className="nav-link"
+          activeClassName="active-link"
+        >
           Observatory
         </NavLink>
-        <NavLink to="/calendar" className="nav-link" activeClassName="active-link">
+        <NavLink
+          to="/calendar"
+          className="nav-link"
+          activeClassName="active-link"
+        >
           Calendar
         </NavLink>
         <NavLink to="/quiz" className="nav-link" activeClassName="active-link">
           Quiz
         </NavLink>
-        <NavLink to="/gallery" className="nav-link" activeClassName="active-link">
+        <NavLink
+          to="/gallery"
+          className="nav-link"
+          activeClassName="active-link"
+        >
           Gallery
         </NavLink>
       </div>
